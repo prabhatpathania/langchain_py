7b478ddcc7957fc1a51e177a2c52ed0d3b790db8
prabhatpathania <prabhatpathania19@gmail.com>
2023-11-27 18:27:00 +0100

    Prompt update

--- src/translate_single.py
+++ src/translate_single.py
@@ -8,17 +8,25 @@ import streamlit as st
 os.environ["OPENAI_API_KEY"] = openai_key
 
 
-st.title('Translate your results')
+st.title('Translate and Learn')
 input_text=st.text_input("Enter the text you want to translate to Dutch")
 
 chat = ChatOpenAI()
 
 messages = [
     SystemMessage(
-        content="You are a helpful assistant that translates English to Dutch."
+        content="""You are a helpful assistant that translates English to Dutch for people who do not understand Dutch. 
+                Your job is help translate the given text using child friendly language and writing sentence not longer than 12 words. 
+                If you are unable to translate a text for the user, you will convey the same in a polite tone.
+                For the response to the input, You will : 
+                1. translate the user input to simple Dutch
+                2. In a separate section, in simple English, explain how to pronounce the translated text. 
+                3. In simple english, give 2 examples (as bullet points) of scenarios where dutch people would use such a word/sentence
+                """
     ),
     HumanMessage(content=input_text),
 ]
 
 if input_text:
+
     st.write(chat(messages=messages).content)
