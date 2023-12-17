75b2632bdb69136d82db9c9c1041209fb7301682
prabhatpathania <prabhatpathania19@gmail.com>
2023-12-17 12:41:38 +0100

    Code cleanup

--- src/audio_to_text.py
+++ src/audio_to_text.py
@@ -1,6 +1,3 @@
-# `pip3 install assemblyai` (macOS)
-# `pip install assemblyai` (Windows)
-
 import os
 from constants import openai_key, aai_key
 from langchain.chat_models import ChatOpenAI
@@ -10,20 +7,18 @@ import assemblyai as aai
 import streamlit as st
 
 os.environ["OPENAI_API_KEY"] = openai_key
-
-st.title('LangChain Demo')
+aai.settings.api_key = aai_key
 
 ## Input link
+st.title('LangChain Demo')
 input_link=st.text_input("Enter the link to audio file")
 
-
-aai.settings.api_key = aai_key
-transcriber = aai.Transcriber()
 ## Transcribe the input file/link
+transcriber = aai.Transcriber()
 transcript = transcriber.transcribe(input_link)
 
-chat = ChatOpenAI()
 ## Prompt
+chat = ChatOpenAI()
 messages = [
     SystemMessage( 
         content="""You are a customer service support bot that goes through the transcript of customer service recorded calls.
@@ -31,7 +26,6 @@ messages = [
                 1. Name of the product
                 2. The problem with the product in less than 50 words
                 3. Type of customer request(repair or refund).
-                
                 """
     ),
     HumanMessage(content=transcript),
